e584766f069f803d36cb5e5b8aba2fa05ec9c855
Quaz <austinhoot7@gmail.com>
2022-11-14 14:41:56 -0500

    update

--- js/index.js
+++ js/index.js
@@ -5,7 +5,14 @@ document.getElementById("StartTime").innerHTML = "Start Time: " + StartTime;
 function updateTime(){
     var timething = new Date();
     let CurrentTime = timething.getHours() + ":" + timething.getMinutes() + ":" + timething.getSeconds();
-    document.getElementById("ElapsedTime").innerHTML = "Current Time: " + CurrentTime;
+    document.getElementById("CurrentTime").innerHTML = "Current Time: " + CurrentTime;
 };
 
-setInterval(updateTime, 1000)
+let ElapsedTime = 0;
+function updateElapsedTime(){
+    ElapsedTime+=1;
+    document.getElementById("ElapsedTime").innerHTML="Elapsed Time: "+ElapsedTime;
+}
+
+setInterval(updateTime, 1000);
+setInterval(updateElapsedTime, 1000)
