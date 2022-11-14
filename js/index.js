6d563d0cca40b557de3dd9ecb18e658929df6312
Quaz <austinhoot7@gmail.com>
2022-11-14 14:04:33 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
 var today = new Date();
 var StartTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 document.getElementById("StartTime").innerHTML = StartTime;
-while(false){
-    setTimeout(function() {
-        var CurrentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        document.getElementById("ElapsedTime").innerHTML = CurrentTime;
-      }, 1000);
+while(true){
+    sleep(1000);
+    var CurrentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    document.getElementById("ElapsedTime").innerHTML = CurrentTime;
 }
