5cde19348f41fd97ba108bd0227bcf24789b6d84
Quaz <austinhoot7@gmail.com>
2022-11-15 14:46:13 -0500

    grid coordinates

--- snake/script.js
+++ snake/script.js
@@ -1,13 +1,14 @@
 const grid = document.getElementById("grid");
 
-function makeRows(rows, cols) {
-  grid.style.setProperty('--grid-rows', rows);
-  grid.style.setProperty('--grid-cols', cols);
-   for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    // cell.innerText = (c + 1);
-    grid.appendChild(cell).className = "grid-item";
-  };
+function makeGrid(rows, cols) {
+    grid.style.setProperty('--grid-rows', rows);
+    grid.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        // cell.innerText = (c + 1);
+        grid.appendChild(cell).className = "grid-item";
+        grid.appendChild(cell).id = "("+(c-(Math.floor(c/cols)*cols))+","+Math.floor(c/cols)+")";
+    };
 };
 
-makeRows(10, 10);
+makeGrid(10, 16);
