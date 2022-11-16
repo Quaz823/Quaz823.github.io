f5e61c77f4b85e014e54852b2c3a5a9e9d7d27b9
Quaz <austinhoot7@gmail.com>
2022-11-16 11:45:57 -0500

    Update script.js

--- camera/script.js
+++ camera/script.js
@@ -4,6 +4,7 @@ video.setAttribute('autoplay', 'true');
 video.setAttribute('muted', 'false');
 video.style.width = '100%';
 video.style.height = '100%';
+video.setAttribute('src','192.168.1.213')
 
 /* Setting up the constraint */
 var facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
