960093ff2a912790a0ea076eb60685f60efe6c45
Quaz <austinhoot7@gmail.com>
2022-11-16 11:26:22 -0500

    camera

--- snake/script.js
+++ snake/script.js
@@ -1,4 +1,7 @@
 const grid = document.getElementById("grid");
+const html = document.getElementById("html")
+
+var square;
 
 function makeGrid(cols, rows) {
     grid.style.setProperty('--grid-rows', rows);
@@ -9,13 +12,44 @@ function makeGrid(cols, rows) {
         grid.appendChild(cell).className = "grid-item";
         grid.appendChild(cell).id = (c-(Math.floor(c/cols)*cols))+","+Math.floor(c/cols);
     };
+    return c;
 };
 
-function colorSquare(x, y) {
-    let coordinates = toString(x+','+y);
-    document.getElementById('4,4').style.backgroundColor = "green";
+function snakeSquare(x, y, toggle=true) {
+    let coordinates = x+','+y;
+    if (toggle) {
+        document.getElementById(coordinates).style.backgroundColor = "green";
+    } else {
+        document.getElementById(coordinates).style.backgroundColor = "white";
+    }
+};
+
+function snakeLocate(){
+
 }
 
-makeGrid(16, 9);
+function snakeMove(directionKey){
+    if (directionKey == w){
+        
+    }
+    if (directionKey == a){
 
-colorSquare(4,4);
+    }
+    if (directionKey == d){
+
+    }
+    if (directionKey == s){
+
+    }
+}
+
+square = makeGrid(16, 9);
+console.log(square)
+
+snakeSquare(4,4);
+
+html.addEventListener('keydown', (e) => {
+    if (!e.repeat) {
+        console.log('Key '+e.key+' pressed [event: keydown]');
+    }
+});
