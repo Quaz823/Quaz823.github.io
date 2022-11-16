b0206fe0ed2650562e85674740c8cfe99cc529c8
Quaz <austinhoot7@gmail.com>
2022-11-16 11:33:55 -0500

    Update script.js

--- camera/script.js
+++ camera/script.js
@@ -1,20 +1,20 @@
 var video = document.createElement('video');
-video.setAttribute('playsinline', '');
-video.setAttribute('autoplay', '');
-video.setAttribute('muted', '');
+video.setAttribute('playsinline', 'true');
+video.setAttribute('autoplay', 'true');
+video.setAttribute('muted', 'false');
 video.style.width = '200px';
 video.style.height = '200px';
 
 /* Setting up the constraint */
 var facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
 var constraints = {
-  audio: false,
-  video: {
-   facingMode: facingMode
-  }
+    audio: true,
+    video: {
+    facingMode: facingMode
+    }
 };
 
 /* Stream it to video element */
 navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-  video.srcObject = stream;
+    video.srcObject = stream;
 });
