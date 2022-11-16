e6e9b7b4dc1a827c071d64480be81d5b991d83f2
Quaz <austinhoot7@gmail.com>
2022-11-16 08:48:49 -0500

    grid update

--- snake/script.js
+++ snake/script.js
@@ -1,14 +1,16 @@
 const grid = document.getElementById("grid");
 
-function makeGrid(rows, cols) {
+function makeGrid(cols, rows) {
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        // cell.innerText = (c + 1);
+        cell.innerText = "("+(c-(Math.floor(c/cols)*cols))+","+Math.floor(c/cols)+")";
         grid.appendChild(cell).className = "grid-item";
-        grid.appendChild(cell).id = "("+(c-(Math.floor(c/cols)*cols))+","+Math.floor(c/cols)+")";
+        grid.appendChild(cell).id = (c-(Math.floor(c/cols)*cols))+","+Math.floor(c/cols);
     };
 };
 
-makeGrid(10, 16);
+makeGrid(16, 9);
+
+document.getElementById('4,4')
